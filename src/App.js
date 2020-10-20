014c388b292e5b9c52a0bb027d5e9dfc7a872544
Mathew Chanda <mvchanda@ncsu.edu>
2020-10-19 20:48:49 -0400

    Added routing to the three pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Switch,Route,BrowserRouter as Router} from 'react-router-dom'; 
 
 function App() {
   return (
     <div>
-      
+      <Router>
+          <Switch>
+            
+            <Route exact path="/">
+              <h1>Home</h1>
+            </Route>
+
+            <Route path="/about">
+              <h1>about</h1>
+            </Route>
+
+            <Route path="/dashboard">
+              <h1>dashboard</h1>
+            </Route>  
+
+        </Switch>
+      </Router>
     </div>
   );
 }
