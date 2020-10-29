31166b233b69872f3395d0636582f321cffdea22
Mathew Chanda <mvchanda@ncsu.edu>
2020-10-28 22:35:25 -0400

    Increased the number of articles being shown

--- src/Component/NewsCard/Articles.js
+++ src/Component/NewsCard/Articles.js
@@ -2,7 +2,7 @@
     The following function returns a promoise that contains JSON of the http reponse https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=1caaf78acfca4d9eb8476f35785f785d
 */ 
 export async function getArticles(category,keyword){
-    return fetch(`https://newsapi.org/v2/top-headlines?country=us&q=${keyword}&category=${category}&apiKey=78b9d599c4f94f8fa3afb1a5458928d6`)
+    return fetch(`https://newsapi.org/v2/top-headlines?country=us&q=${keyword}&category=${category}&pageSize=100&apiKey=78b9d599c4f94f8fa3afb1a5458928d6`)
         .then(response => response.json())
 }
 
